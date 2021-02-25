6d2e94bd7e8da53004777a6c8cd03f460d0e2ef7
Wendy <wsu315@gmail.com>
2021-02-25 01:34:53 -0800

    Updates

---
+++ precache-manifest.56f3a5fa130f676f6791356d38f554ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2f2bb1426d223849ae24f57f9ea8a9",
+    "url": "/kwk-camera-roll-bingo/index.html"
+  },
+  {
+    "revision": "7da0bda5a3d431a49389",
+    "url": "/kwk-camera-roll-bingo/static/css/main.7f83aff8.chunk.css"
+  },
+  {
+    "revision": "8b1ffa4ccca7e69ee65b",
+    "url": "/kwk-camera-roll-bingo/static/js/2.fadbdd3a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kwk-camera-roll-bingo/static/js/2.fadbdd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da0bda5a3d431a49389",
+    "url": "/kwk-camera-roll-bingo/static/js/main.174cd309.chunk.js"
+  },
+  {
+    "revision": "6836da44089a6f211e2e",
+    "url": "/kwk-camera-roll-bingo/static/js/runtime-main.9d71501c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kwk-bingo/precache-manifest.86e5a71fc990461b96a4b9d799b391b9.js"
+  "/kwk-camera-roll-bingo/precache-manifest.56f3a5fa130f676f6791356d38f554ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kwk-bingo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kwk-camera-roll-bingo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
